1566703366b0f9174074245165a552a52fd8580f
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-24 12:21:24 +0600

    purchase page implemented

--- src/Pages/PlaceOrder/PlaceOrder.js
+++ src/Pages/PlaceOrder/PlaceOrder.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
+import useTools from '../../hooks/useTools';
+import Loading from '../Shared/Loading';
+import './PlaceOrder.css';
 
 const PlaceOrder = () => {
     const location = useLocation();
-    const _id = location?.state?._id;
+    const id = location?.state?._id;
+    const [tools, loading] = useTools();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    if (loading) {
+        return;
+    }
+    const selectedTool = tools?.find(tool => tool._id === id);
+    const { _id, name, minimumOrderQuantity, availableQuantity, unitPrice } = selectedTool || {};
+    const handlePurchaseOrder = event => {
+        event.preventDefault();
+        const quantity = event.target.quantity.value;
+        const phone = event.target.phone.value;
+        const address = event.target.address.value;
+        if (parseInt(quantity) < parseInt(minimumOrderQuantity)) {
+            setErrorMessage(`Minimum order quantity is ${minimumOrderQuantity}`);
+        }
+        else if (parseInt(quantity) > parseInt(availableQuantity)) {
+            setErrorMessage(`Sorry! Available tools are ${availableQuantity}`);
+        }
+        else if (phone.length !== 11) {
+            setErrorMessage('Phone number must be 11 digits.')
+        }
+        else {
+            setErrorMessage('');
+        }
+    }
     return (
-        <div>
-            <h2>place order</h2>
+        <div className='w-50 mx-auto my-5 py-2'>
+            <h2 className='text-center display-6'>Place order for {name}</h2>
+            <p className="text-center mb-2">
+                <small className="me-3 text-center">Minimum Order Quantity: {minimumOrderQuantity}</small>
+                <small className="me-3 text-center">Available Quantity: {availableQuantity}</small>
+                <small className="m-0 text-center">Unit Price: ${unitPrice}</small>
+            </p>
+
+            <form onSubmit={handlePurchaseOrder}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" aria-describedby="nameHelp" defaultValue={'props.firstName'} readOnly disabled />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" defaultValue={'props@hghga.com'} readOnly disabled />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="quantity" className="form-label">Order Quantity</label>
+                    <input type="number" name="quantity" className="form-control" id="quantity" aria-describedby="quantityHelp" placeholder='Please Enter Quantity' onWheel={event => event.target.blur()} required />
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="phone" className="form-label">Phone Number</label>
+                    <input type="number" name="phone" className="form-control" id="phone" aria-describedby="phoneHelp" placeholder='Please Enter Phone Number' onWheel={event => event.target.blur()} required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="address" className="form-label">Address</label>
+                    <input type="text" name="address" className="form-control" id="address" aria-describedby="addressHelp" placeholder='Please Enter Address' required />
+                </div>
+                {errorMessage && <p className="text-danger">{errorMessage}</p>}
+                <input className='btn btn-secondary d-block w-50 mx-auto mt-2' type="submit" />
+            </form>
         </div>
     );
 };
